ed672151fbbbd47de2745a7860361c4b56e77d92
castrodiegoa <diegocastroac123@gmail.com>
2025-04-10 10:58:12 -0500

    first commit

---
+++ config/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
+
+# Configuración Oracle
+ORACLE_CONFIG = {
+    "user": os.getenv("ORACLE_USER"),
+    "password": os.getenv("ORACLE_PASSWORD"),
+    "dsn": os.getenv("ORACLE_DSN"),
+}
+
+# Configuración PostgreSQL
+POSTGRES_CONFIG = {
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT"),
+    "database": os.getenv("POSTGRES_DATABASE"),
+}
